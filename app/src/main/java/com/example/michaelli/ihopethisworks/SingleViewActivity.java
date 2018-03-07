721f7bb6e24ceca0e1aa7d0fb5434da881799e4b
Michael Li <60745137>
2018-03-07 09:32:24 -0800

    Initial commit

---
+++ app/src/main/java/com/example/michaelli/ihopethisworks/SingleViewActivity.java
@@ -0,0 +1,30 @@
+package com.example.michaelli.ihopethisworks;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SingleViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_view);
+        Intent i = getIntent();
+        // Selected image id
+        int position = i.getExtras().getInt("id");
+        ImageAdapter imageAdapter = new ImageAdapter(this);
+
+        ImageView imageView = (ImageView) findViewById(R.id.SingleView);
+        imageView.setImageResource(imageAdapter.mThumbIds[position]);
+
+        TextView tv = (TextView) findViewById(R.id.text);
+        tv.setText(imageAdapter.two[position]);
+
+        TextView tv2 = (TextView) findViewById(R.id.text2);
+        tv2.setText(imageAdapter.three[position]);
+
+    }
+}
