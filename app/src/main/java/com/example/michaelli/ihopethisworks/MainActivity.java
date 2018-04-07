06226d025b43515b82812167bfd5a6d15a43a7e7
Justin <blob4444333221@gmail.com>
2018-04-06 18:23:45 -0700

    added button for menu

--- app/src/main/java/com/example/michaelli/ihopethisworks/MainActivity.java
+++ app/src/main/java/com/example/michaelli/ihopethisworks/MainActivity.java
@@ -5,14 +5,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +29,7 @@ public class MainActivity extends Activity {
 
         gridview.setOnItemClickListener(new OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent,
-                                    View v, int position, long id){
+                                    View v, int position, long id) {
                 // Send intent to SingleViewActivity
                 Intent i = new Intent(getApplicationContext(), SingleViewActivity.class);
                 // Pass image index
@@ -32,5 +38,19 @@ public class MainActivity extends Activity {
                 Intent b = new Intent(getApplicationContext(), Cust2.class);
             }
         });
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return  super.onOptionsItemSelected(item);
     }
 }
