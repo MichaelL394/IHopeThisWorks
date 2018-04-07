4f742322dec69feef13069f270528b73c2099387
Justin <blob4444333221@gmail.com>
2018-04-06 17:44:39 -0700

    really adding pullout menu

---
+++ app/src/main/java/com/example/michaelli/ihopethisworks/SquareView.java
@@ -0,0 +1,30 @@
+package com.example.michaelli.ihopethisworks;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class SquareView extends android.support.v7.widget.AppCompatImageView {
+
+    public SquareView(Context context) {
+        super(context);
+    }
+
+    public SquareView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (widthMeasureSpec < heightMeasureSpec) {
+            super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+        } else if (heightMeasureSpec < widthMeasureSpec) {
+            super.onMeasure(heightMeasureSpec, heightMeasureSpec);
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+}
