5800496cc36a7ad3bb918893ba0ec24de656abf5
michael <batman3940@gmail.com>
2018-04-16 17:13:25 -0700

    beginning screen added

--- app/src/main/java/com/example/michaelli/ihopethisworks/begginingScreen.java
+++ app/src/main/java/com/example/michaelli/ihopethisworks/begginingScreen.java
@@ -1,13 +1,48 @@
 package com.example.michaelli.ihopethisworks;
 
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 public class begginingScreen extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beggining_screen);
+
+
+        mToolBar = (Toolbar) findViewById(R.id.nav_action);
+        setSupportActionBar(mToolBar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.firstScreen);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    /** Called when the user taps the Send button */
+    public void goToMeatsAndBeans(View view) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return  super.onOptionsItemSelected(item);
     }
 }
